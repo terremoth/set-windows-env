b2511571e9f8db65eb32d6dc993a22d7c18fecdc
Lucas M. Dutra <terremoth@users.noreply.github.com>
2018-04-10 13:45:28 -0300

    Create create-env.bat

---
+++ create-env.bat
@@ -0,0 +1,67 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install jre9 -y
+
+choco install firefox -y 
+
+choco install sublimetext3 -y
+
+choco install sumatrapdf -y
+
+choco install python -y
+
+choco install flashplayerplugin -y
+
+choco install mysql.workbench -y
+
+choco install visualstudiocode -y
+
+choco install curl -y
+
+choco install putty -y
+
+choco install php -y
+
+choco install nodejs.install -y 
+
+choco install libreoffice -y 
+
+choco install wget -y 
+
+choco install googlechrome -y 
+
+choco install 7zip.install -y 
+
+choco install skype -y 
+
+choco install git.install -y 
+
+choco install winrar -y 
+
+choco install notepadplusplus -y 
+
+choco install yarn -y 
+
+choco install adobeshockwaveplayer -y 
+
+choco install putty.install -y 
+
+choco install openssh -y 
+
+choco install visualstudio2015community -y
+
+choco install cpu-z.install -y
+
+choco install rufus -y 
+
+choco install composer -y
+
+choco install selenium-chrome-driver -y 
+
+choco install nginx -y
+
+choco install vlc -y 
+
+choco install mariadb -y
+
+choco install postman -y 
