b4bd9f5a3b71309631cdcde13f6a3eb0852456de
Lucas M. Dutra <terremoth@users.noreply.github.com>
2018-04-10 15:21:24 -0300

    Update create-env.bat

--- create-env.bat
+++ create-env.bat
@@ -1,6 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install jre9 -y
+choco install jre8 -y
 
 choco install firefox -y 
 
@@ -65,3 +65,5 @@ choco install vlc -y
 choco install mariadb -y
 
 choco install postman -y 
+
+choco install netbeans -y
